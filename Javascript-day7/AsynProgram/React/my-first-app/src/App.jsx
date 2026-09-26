d8b495a666ec6383fe39a1afa926df40bc10726a
jeevitha-codes16 <jeevithavasanth2k7@gmail.com>
2026-09-26 22:51:58 +0530

    app

---
+++ Javascript-day7/AsynProgram/React/my-first-app/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.'
+
+function App(){
+
+return(
+  //HTML
+  //REACT CLOSE ALL THE HTML TAG
+  // <Tag Name-Input /Img /br />
+  <div>
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>support</li>
+      </ul>
+    </nav>
+
+    <h1>This is react session</h1>
+    <p>Good evening</p>
+    <button>Greet</button>
+
+    <input type="text" placeholder="enter your name" />
+  </div>
+)
+
+}
+
+export default App
